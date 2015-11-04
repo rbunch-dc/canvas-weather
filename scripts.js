5cfda404e5239dc031f1076bdffe29504b1c1377
Robert Bunch <robert.bunch@tinroofsoftware.com>
2015-11-04 13:48:50 -0500

    Update to include Json feed from open weather map

--- scripts.js
+++ scripts.js
@@ -1,48 +1,71 @@
 $(document).ready(function($){
 
-	var currTemp = 80.0;
-	var canvas = $('#weather-canvas');
-	var context = canvas[0].getContext('2d');
+	var apikey = 'YOURAPIKEY';
+	var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=Atlanta,ga&units=imperial&APPID="+apikey;
 
-	// context.beginPath();
-	// context.moveTo(0,0);
-	// context.lineTo(100,100);
-	// context.moveTo(20,100);
-	// context.lineTo(20,50);
-	// context.lineWidth = 10;
-	// context.stroke();
+	$.getJSON(weatherUrl, function(weatherData){
+		console.log(weatherData);
+		var currTemp = weatherData.main.temp;
 
-	var lineWidth = 5;
-	var outterRadius = 70;
-	var innerRadius = outterRadius - lineWidth;
-	var currPerc = 0;
-	var counterClockwise = false;
-	var circ = Math.PI * 2;
-	var quart = Math.PI / 2;
+		var canvas = $('#weather-canvas');
+		var context = canvas[0].getContext('2d');
 
-function animate(current){
+		// context.beginPath();
+		// context.moveTo(0,0);
+		// context.lineTo(100,100);
+		// context.moveTo(20,100);
+		// context.lineTo(20,50);
+		// context.lineWidth = 10;
+		// context.stroke();
 
-	context.fillStyle = "#ccc";
-	context.beginPath();
-	context.arc(155,75,innerRadius,0,2*Math.PI,true);
-	context.closePath();
-	context.fill();
+		var lineWidth = 5;
+		var outterRadius = 70;
+		var innerRadius = outterRadius - lineWidth;
+		var currPerc = 0;
+		var counterClockwise = false;
+		var circ = Math.PI * 2;
+		var quart = Math.PI / 2;
+		var shadeColor;
 
-	context.lineWidth = 10;
-	context.strokeStyle = '#E3170D';
-	context.beginPath();
-	context.arc(155, 75, outterRadius, -(quart), ((circ) * current) - quart, false);
-	context.stroke();
-	currPerc++;
-	if(currPerc < currTemp){
-		requestAnimationFrame(function () {
-			animate(currPerc / 100);
-		});
+		if(currTemp < 32){
+			shadeColor = '#D4F0FF';
+		}else if((currTemp >= 32) && (currTemp < 59)){
+			shadeColor = "#129793";
+		}else if((currTemp >= 59) && (currTemp < 75)){
+			shadeColor = "#7cfc00";
+		}else if((currTemp >= 75) && (currTemp < 90)){
+			shadeColor = "#FF6600";
+		}else{
+			shadeColor = '#E3170D';
+		}
+	function animate(current){
+
+		context.fillStyle = "#ccc";
+		context.beginPath();
+		context.arc(155,75,innerRadius,0,2*Math.PI,true);
+		context.closePath();
+		context.fill();
+
+		context.lineWidth = 10;
+		context.strokeStyle = shadeColor;
+		context.beginPath();
+		context.arc(155, 75, outterRadius, -(quart), ((circ) * current) - quart, false);
+		context.stroke();
+		context.font = "48px Myriad Pro";
+		context.fillStyle = "Blue";
+		context.textBaseline = "top";
+		context.fillText(currTemp,175-outterRadius, 85-outterRadius/2);
+		currPerc++;
+		if(currPerc < currTemp){
+			requestAnimationFrame(function () {
+				animate(currPerc / 100);
+			});
+		}
 	}
-}
 
-animate();
-context.closePath();
+	animate();
+	context.closePath();
+	});
 
 });
 
